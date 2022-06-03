211933aa5cf44818b31c0e814e901afb2536cd4a
Garrett Hays <GarrettlHays@gmail.com>
2022-06-03 11:51:56 -0700

    add if else functionality for branching logic

---
+++ js/scripts.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+    const userName = $("input#name").val();
+    const userSelect1 = $("select#selectOne").val();
+    const userSelect2 = $("select#selectTwo").val();
+    const userSelect3 = $("select#selectThree").val();
+    const userSelect4 = $("select#selectFour").val();
+    const userSelect5 = $("select#selectFive").val();
+
+    $(".name").text(userName);
+
+    if (userSelect1 === "backEnd") {
+      $("#resultCS").show();
+      $("#resultCS").siblings().hide();
+
+      } else {
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-cs") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-js") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-js") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-js") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-js") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-js") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-csjs" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-js") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-cs") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-cs") {
+          $("#resultCS").show();
+          $("#resultCS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-cs") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-cs") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-js") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-js") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-cs" && userSelect5 === "selectFive-js") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-cs" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-js") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-js" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-js") {
+          $("#resultJS").show();
+          $("#resultJS").siblings().hide();
+        }
+        if (userSelect2 === "selectTwo-python" && userSelect3 === "selectThree-python" && userSelect4 === "selectFour-python" && userSelect5 === "selectFive-js") {
+          $("#resultPython").show();
+          $("#resultPython").siblings().hide();
+        }
+      }
+  });
+});
